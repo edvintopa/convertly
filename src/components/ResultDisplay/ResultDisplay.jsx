572ca1b7aca887c9331cc2fe82e65845ed064e60
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-05-29 02:26:32 +0200

    typewriter animation for results

--- src/components/ResultDisplay/ResultDisplay.jsx
+++ src/components/ResultDisplay/ResultDisplay.jsx
@@ -1,12 +1,22 @@
+import { motion } from 'framer-motion';
+
 function ResultDisplay({ amount, fromCurrency, toCurrency, convertedAmount }) {
+    
     return (
         <div className="text-center py-8">
-            <p className="text-5xl">
+            <motion.p 
+                className="text-5xl"
+                key={amount}
+                initial={{ width: 0 }}
+                animate={{ width: "auto" }}
+                transition={{ duration: 0.2 }}
+                style={{ overflow: "hidden", whiteSpace: "nowrap" }}
+            >
                 <span className="text-8xl font-bold">{amount}</span>{" "}
                 <span>{fromCurrency}</span> = {" "}
                 <span className="text-8xl font-bold">{convertedAmount}</span>{" "}
                 <span>{toCurrency}</span>
-            </p>
+            </motion.p>
         </div>
     );
 }
